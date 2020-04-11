6bd8308169b4436656214d79bcbb608e30bc6572
Mauro Bianchi <bianchimro@gmail.com>
2020-04-11 19:14:44 +0200

    more aggregation expressions: first, last, count, countDistinct, csvDistinct, tsvDistinct

--- src/expressionRegister.js
+++ src/expressionRegister.js
@@ -3,8 +3,11 @@ import mean from 'lodash/mean'
 import max from 'lodash/max'
 import min from 'lodash/min'
 import sum from 'lodash/sum'
+import first from 'lodash/first'
+import last from 'lodash/last'
 import isFunction from 'lodash/isFunction'
 import isString from 'lodash/isString'
+import uniq from 'lodash/uniq'
 
 const aggregationsRegister = {}
 
@@ -33,6 +36,12 @@ export function getAggregator(aggregator){
 }
 
 // Aggregators available in RAW
+// general purpose
+registerAggregation("last", last)
+registerAggregation("first", first)
+registerAggregation("count", items => items.length)
+registerAggregation("countDistinct", items => uniq(items).length)
+
 // numbers
 registerAggregation("mean", mean)
 registerAggregation("max", max)
@@ -40,3 +49,14 @@ registerAggregation("min", min)
 registerAggregation("sum", sum)
 
 //string
+const commaSeparated = items => items.join(",")
+const tabSeparated = items => items.join("\t")
+const newLineSeparated = items => items.join("\n")
+
+registerAggregation("csv", commaSeparated)
+registerAggregation("csvDistinct", items => commaSeparated(uniq(items)))
+registerAggregation("commaSeparated", commaSeparated)
+registerAggregation("tsv", tabSeparated)
+registerAggregation("tsvDistinct", items => tabSeparated(uniq(items)))
+registerAggregation("tabSeparated", tabSeparated)
+registerAggregation("newLineSeparated", newLineSeparated)
