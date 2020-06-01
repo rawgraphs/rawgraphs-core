1c565c4ab47caf44115ba3036aabc58eb577043e
uf0 <scnepa222@gmail.com>
2020-06-01 11:52:20 +0200

    update webbox

--- sandbox/web/index.js
+++ sandbox/web/index.js
@@ -1,3 +1,29 @@
-import {raw} from 'raw'
+import { chart } from "../../src";
+import testChart from "../../src/testChart";
 
-console.log(123, raw)
+const div = document.querySelector("#root");
+
+const testData = [
+  { x: 10, y: 20 },
+  { x: 30, y: 50 },
+  { x: 100, y: 20 },
+  { x: 50, y: 70 },
+];
+
+const dispersionMapping = {
+  x: {
+    value: "x",
+  },
+  y: {
+    value: "y",
+  },
+};
+
+const viz = chart(testChart, {
+  data: testData,
+  mapping: dispersionMapping,
+  dataTypes: {},
+  visualOptions: {},
+});
+
+viz.renderToDOM(div);
