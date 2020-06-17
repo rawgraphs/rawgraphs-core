ea22e0898144a4fa364fcfa94e04ec271eadab6b
Mauro Bianchi <bianchimro@gmail.com>
2020-06-18 01:12:06 +0200

    exporting functions from expressionRegister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as chart } from './raw'
 export { parseDataset, inferTypes } from './dataset'
 export { default as makeMapper, validateMapping, validateMapperDefinition } from './mapping'
+export { registerAggregation, unregisterAggregation } from './expressionRegister'
