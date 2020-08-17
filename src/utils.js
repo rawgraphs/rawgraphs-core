38a09b443b58df9556c10efa23c2b7e54186af72
Mauro Bianchi <bianchimro@gmail.com>
2020-08-17 16:49:18 +0200

    fixed type check for object based type definition

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,43 @@
+import isPlainObject from "lodash/isPlainObject";
+import isString from "lodash/isString";
+
 export class RAWError extends Error {
   constructor(message) {
-    super(message)
-    this.name = 'RAWError'
-    this.message = message
+    super(message);
+    this.name = "RAWError";
+    this.message = message;
   }
 }
-
-
 
 export class ValidationError extends Error {
   constructor(errors) {
     //super(message)
-    super('validation error')
-    this.name = 'ValidationError'
-    this.errors = errors
+    super("validation error");
+    this.name = "ValidationError";
+    this.errors = errors;
   }
+}
+
+export function getType(dataType) {
+  if (isPlainObject(dataType)) {
+    return getType(dataType.type);
+  }
+
+  if (isString(dataType)) {
+    switch (dataType.toLowerCase()) {
+      case "string":
+        return String;
+      case "number":
+        return Number;
+      case "boolean":
+        return Boolean;
+      case "date":
+        return Date;
+
+      default:
+        return String;
+    }
+  }
+
+  return dataType;
 }
