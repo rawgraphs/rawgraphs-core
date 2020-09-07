98961420b6a77d3f802266b2f11225469b3e4164
uf0 <scnepa222@gmail.com>
2020-09-07 17:30:42 +0200

    improve legend

--- src/legend.js
+++ src/legend.js
@@ -19,9 +19,10 @@ export function rawgraphsLegend(
   shapePadding = 5,
   shapeWidth = 15,
   shapeHeight = 15,
-  margin = { top: 20, right: 5, bottom: 0, left: 5 }
+  margin = { top: 0, right: 5, bottom: 0, left: 5 }
 ) {
   const legend = (_selection) => {
+    let d3LegendSize, d3legendColor;
     const w = legendWidth - margin.left - margin.right;
 
     const legendContainer = _selection
@@ -35,7 +36,7 @@ export function rawgraphsLegend(
         .attr("class", "legendSize")
         .attr("transform", `translate(0,${margin.top})`);
 
-      var d3LegendSize = d3Legend
+      let d3LegendSize = d3Legend
         .legendSize()
         .scale(legendSize.scale)
         .cells(legendSize.scale.domain())
@@ -56,18 +57,14 @@ export function rawgraphsLegend(
     if (legendColor && legendColor.title) {
       const legendColorHeight = legendContainer.select(".legendSize").empty()
         ? 0
-        : legendContainer.select(".legendSize").node().getBBox().height +
-          margin.top;
+        : legendContainer.select(".legendSize").node().getBBox().height + 20;
 
       legendContainer
         .append("g")
         .attr("class", "legendColor")
-        .attr(
-          "transform",
-          "translate(0," + (legendColorHeight + margin.top) + ")"
-        );
+        .attr("transform", "translate(0," + legendColorHeight + ")");
 
-      const d3legendColor = d3Legend
+      d3legendColor = d3Legend
         .legendColor()
         .shapePadding(shapePadding)
         .title(legendColor.title)
@@ -84,6 +81,7 @@ export function rawgraphsLegend(
           .shapeHeight(10)
           .cells(w)
           .classPrefix("horizontal-")
+          .labelAlign("start")
           .labels(({ i, genLength, generatedLabels, domain }) => {
             if (i === 0 || i === genLength - 1) {
               return generatedLabels[i];
@@ -96,6 +94,42 @@ export function rawgraphsLegend(
 
       legendContainer.select(".legendColor").call(d3legendColor);
     }
+
+    //Hardcore style with much love
+    legendContainer
+      .selectAll("text")
+      .attr("font-family", '"Arial", sans-serif')
+      .attr("font-size", "10px");
+
+    legendContainer
+      .selectAll(".legendTitle")
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold");
+
+    legendContainer
+      .selectAll(".horizontal-legendTitle")
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold");
+
+    legendContainer
+      .selectAll(".horizontal-cell:first-of-type text")
+      .style("text-anchor", "start")
+      .attr("text-anchor", "start");
+
+    legendContainer
+      .selectAll(".horizontal-cell:last-of-type text")
+      .style("text-anchor", "end")
+      .attr("text-anchor", "end");
+
+    legendContainer
+      .selectAll(".legendSize circle")
+      .attr("fill", "none")
+      .attr("stroke", "#ccc");
+
+    legendContainer
+      .selectAll(".legendSize rect")
+      .attr("fill", "none")
+      .attr("stroke", "#ccc");
   };
 
   legend.addColor = function (_title, _scale) {
@@ -105,13 +139,12 @@ export function rawgraphsLegend(
     return legend;
   };
 
-  legend.addSize = function (_title, _scale, _shape, _extent) {
+  legend.addSize = function (_title, _scale, _shape) {
     if (!arguments.length) return legendSize;
     legendSize = {
       title: _title,
       scale: _scale,
       shape: _shape,
-      extent: _extent,
     };
     return legend;
   };
