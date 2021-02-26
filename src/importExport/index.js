c4a2d75c076fc6eb1b059a0fae707978199caf2f
Alberto Osio <osioalberto@gmail.com>
2021-02-26 12:46:44 +0100

    Add import and export (multiversion), closes #23

---
+++ src/importExport/index.js
@@ -0,0 +1,51 @@
+import * as V1 from "./importExportV1"
+import * as V1_1 from "./importExportV1.1"
+import get from "lodash/get"
+import keyBy from "lodash/keyBy"
+
+const DESERIALIZERS = keyBy([V1, V1_1], "VERSION")
+
+export function serializeProject(project, version = "latest") {
+  const defaultSerializer =
+    version === "latest"
+      ? DESERIALIZERS[V1_1.VERSION].serializeProject
+      : () => {
+          throw new Error("No serializer found for version " + version)
+        }
+  const serializer = get(
+    DESERIALIZERS,
+    version + ".serializeProject",
+    defaultSerializer
+  )
+  return serializer(project)
+}
+
+export function deserializeProject(serializedProject, charts) {
+  try {
+    const parsedProject = JSON.parse(serializedProject)
+    const version = get(parsedProject, "version", "unknown")
+    if (DESERIALIZERS[version]) {
+      try {
+        return DESERIALIZERS[version].deserializeProject(parsedProject, charts)
+      } catch (e) {
+        throw new Error("Can't open your project. " + e.message)
+      }
+    } else {
+      throw new Error("Can't open your project. Invalid file")
+    }
+  } catch (e) {
+    throw new Error("Can't open your project. " + e.message)
+  }
+}
+
+export function registerSerializerDeserializer(
+  version,
+  serializer,
+  deserializer
+) {
+  DESERIALIZERS[version] = {
+    serializeProject: serializer,
+    deserializeProject: deserializer,
+    VERSION: version,
+  }
+}
